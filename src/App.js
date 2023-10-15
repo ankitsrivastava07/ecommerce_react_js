146698a455d94ffd121ccf8c14e1be3e2f2887af
Ankit <ankitsrivastava7835@gmail.com>
2023-10-15 22:57:41 +0530

    chngs

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './home/Header';
+import Footer from './home/Footer';
+import Slider from './home/Slider';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Slider></Slider>
+
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/home/Footer.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div id="footer">
+            <div class="container" >
+                <footer class="py-5">
+                    <div class="row">
+                        <div class="col-6 col-md-4 mb-3">
+                            <h5>Section</h5>
+                            <ul class="nav flex-column">
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="col-6 col-md-4 mb-3">
+                            <h5>Section</h5>
+                            <ul class="nav flex-column">
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="col-6 col-md-4 mb-3">
+                            <h5>Section</h5>
+                            <ul class="nav flex-column">
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+                            </ul>
+                        </div>
+
+                       
+                    </div>
+                </footer>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
